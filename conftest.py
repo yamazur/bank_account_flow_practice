35c210d9f509042234440f12a944d1862a08a61f
Yana Mazur <mazurihaa@yandex.ru>
2026-02-06 20:49:57 +0300

    update yml

--- conftest.py
+++ conftest.py
@@ -12,9 +12,15 @@ from pages.open_account_page import OpenAccountPage
 
 @pytest.fixture
 def browser():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
+
     service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
-    driver.maximize_window()
+    driver = webdriver.Chrome(service=service, options=options)
+
     yield driver
     driver.quit()
 
